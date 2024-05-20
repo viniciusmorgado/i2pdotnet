551bf4a82dc85bf99a91aa84de762c393dfd7b6f
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-05-20 19:25:20 -0300

    Beginning of support for .NET 8 LTS

--- I2P.NET.SAM/Exceptions/I2PException.cs
+++ I2P.NET.SAM/Exceptions/I2PException.cs
@@ -1,13 +1,7 @@
-﻿namespace I2P.NET.SAM.Exceptions
+﻿namespace I2P.NET.SAM.Exceptions;
+
+internal class I2PException(string message, string errorCode, string rawMessage = null) : Exception()
 {
-    internal class I2PException : Exception
-    {
-        public string ErrorCode { get; }
-        public string RawMessage { get; }
-        public I2PException(string message, string errorCode, string rawMessage = null) : base()
-        {
-            ErrorCode = errorCode;
-            RawMessage = rawMessage;
-        }
-    }
+    public string ErrorCode { get; } = errorCode;
+    public string RawMessage { get; } = rawMessage;
 }
